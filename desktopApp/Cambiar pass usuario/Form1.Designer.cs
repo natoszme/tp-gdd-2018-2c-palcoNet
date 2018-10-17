738bb161a73dbd024667443715f897f0d09a3b2c
natoszme <natoszmedra@gmail.com>
2018-10-17 14:22:00 -0300

    Agrego otro form para el cambio de contrasenia de un admin a un usuario

--- desktopApp/Cambiar pass usuario/Form1.Designer.cs
+++ desktopApp/Cambiar pass usuario/Form1.Designer.cs
@@ -34,7 +34,7 @@
             this.txtRepetirClave = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnCambiar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -70,7 +70,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 90);
+            this.label2.Location = new System.Drawing.Point(30, 90);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(95, 13);
             this.label2.TabIndex = 4;
@@ -85,21 +85,21 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Repetir contraseña";
             // 
-            // button1
+            // btnCambiar
             // 
-            this.button1.Location = new System.Drawing.Point(84, 179);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Cambiar contraseña";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnCambiar.Location = new System.Drawing.Point(84, 179);
+            this.btnCambiar.Name = "btnCambiar";
+            this.btnCambiar.Size = new System.Drawing.Size(118, 23);
+            this.btnCambiar.TabIndex = 6;
+            this.btnCambiar.Text = "Cambiar contraseña";
+            this.btnCambiar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 225);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCambiar);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtRepetirClave);
@@ -121,6 +121,6 @@
         private System.Windows.Forms.TextBox txtRepetirClave;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCambiar;
     }
 }
