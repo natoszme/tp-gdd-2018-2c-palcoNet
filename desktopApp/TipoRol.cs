2d8d92226c489aff4e8467c9a512a66fc4ee3d77
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-10-25 13:25:09 -0300

    Agrego el TipoRol que es un enum de los roles disponibles

---
+++ desktopApp/TipoRol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalcoNet
+{
+     enum TipoRol
+    {
+        Empresa = 0,
+        Administrativo = 1,
+        Cliente = 2
+    }
+    
+}
