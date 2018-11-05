6bd057ee6f1ff744fd41901f25b91d306583123c
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-11-05 17:53:58 -0300

    Agrego los botones de las funcionalidades y el paso a su formulario correspondiente. Falta implementar 3

--- desktopApp/Views/Home.Designer.cs
+++ desktopApp/Views/Home.Designer.cs
@@ -28,11 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAbmRol = new System.Windows.Forms.Button();
+            this.btnRegistroUsuario = new System.Windows.Forms.Button();
+            this.btnAbmEmpresa = new System.Windows.Forms.Button();
+            this.btnAbmClientes = new System.Windows.Forms.Button();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.btnEditarPublicacion = new System.Windows.Forms.Button();
+            this.btnGenerarPublicacion = new System.Windows.Forms.Button();
+            this.btnAbmGrado = new System.Windows.Forms.Button();
+            this.btnModificarContraseña = new System.Windows.Forms.Button();
+            this.btnRendicionComisiones = new System.Windows.Forms.Button();
+            this.btnCanjePuntos = new System.Windows.Forms.Button();
+            this.btnHistorial = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAbmRol
+            // 
+            this.btnAbmRol.Location = new System.Drawing.Point(12, 34);
+            this.btnAbmRol.Name = "btnAbmRol";
+            this.btnAbmRol.Size = new System.Drawing.Size(126, 23);
+            this.btnAbmRol.TabIndex = 0;
+            this.btnAbmRol.Text = "ABM Rol";
+            this.btnAbmRol.UseVisualStyleBackColor = true;
+            this.btnAbmRol.Click += new System.EventHandler(this.btnAbmRol_Click);
+            // 
+            // btnRegistroUsuario
+            // 
+            this.btnRegistroUsuario.Location = new System.Drawing.Point(144, 34);
+            this.btnRegistroUsuario.Name = "btnRegistroUsuario";
+            this.btnRegistroUsuario.Size = new System.Drawing.Size(126, 23);
+            this.btnRegistroUsuario.TabIndex = 1;
+            this.btnRegistroUsuario.Text = "Registrar Usuario";
+            this.btnRegistroUsuario.UseVisualStyleBackColor = true;
+            this.btnRegistroUsuario.Click += new System.EventHandler(this.btnRegistroUsuario_Click);
+            // 
+            // btnAbmEmpresa
+            // 
+            this.btnAbmEmpresa.Location = new System.Drawing.Point(144, 63);
+            this.btnAbmEmpresa.Name = "btnAbmEmpresa";
+            this.btnAbmEmpresa.Size = new System.Drawing.Size(126, 23);
+            this.btnAbmEmpresa.TabIndex = 3;
+            this.btnAbmEmpresa.Text = "ABM Empresa";
+            this.btnAbmEmpresa.UseVisualStyleBackColor = true;
+            this.btnAbmEmpresa.Click += new System.EventHandler(this.btnAbmEmpresa_Click);
+            // 
+            // btnAbmClientes
+            // 
+            this.btnAbmClientes.Location = new System.Drawing.Point(12, 63);
+            this.btnAbmClientes.Name = "btnAbmClientes";
+            this.btnAbmClientes.Size = new System.Drawing.Size(126, 23);
+            this.btnAbmClientes.TabIndex = 2;
+            this.btnAbmClientes.Text = "ABM Clientes";
+            this.btnAbmClientes.UseVisualStyleBackColor = true;
+            this.btnAbmClientes.Click += new System.EventHandler(this.btnAbmClientes_Click);
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.Location = new System.Drawing.Point(144, 121);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(126, 23);
+            this.btnComprar.TabIndex = 7;
+            this.btnComprar.Text = "Comprar Entrada";
+            this.btnComprar.UseVisualStyleBackColor = true;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
+            // btnEditarPublicacion
+            // 
+            this.btnEditarPublicacion.Location = new System.Drawing.Point(12, 121);
+            this.btnEditarPublicacion.Name = "btnEditarPublicacion";
+            this.btnEditarPublicacion.Size = new System.Drawing.Size(126, 23);
+            this.btnEditarPublicacion.TabIndex = 6;
+            this.btnEditarPublicacion.Text = "Editar Publicacion";
+            this.btnEditarPublicacion.UseVisualStyleBackColor = true;
+            this.btnEditarPublicacion.Click += new System.EventHandler(this.btnEditarPublicacion_Click);
+            // 
+            // btnGenerarPublicacion
+            // 
+            this.btnGenerarPublicacion.Location = new System.Drawing.Point(144, 92);
+            this.btnGenerarPublicacion.Name = "btnGenerarPublicacion";
+            this.btnGenerarPublicacion.Size = new System.Drawing.Size(126, 23);
+            this.btnGenerarPublicacion.TabIndex = 5;
+            this.btnGenerarPublicacion.Text = "Generar Publicacion";
+            this.btnGenerarPublicacion.UseVisualStyleBackColor = true;
+            this.btnGenerarPublicacion.Click += new System.EventHandler(this.btnGenerarPublicacion_Click);
+            // 
+            // btnAbmGrado
+            // 
+            this.btnAbmGrado.Location = new System.Drawing.Point(12, 92);
+            this.btnAbmGrado.Name = "btnAbmGrado";
+            this.btnAbmGrado.Size = new System.Drawing.Size(126, 23);
+            this.btnAbmGrado.TabIndex = 4;
+            this.btnAbmGrado.Text = "ABM Grado";
+            this.btnAbmGrado.UseVisualStyleBackColor = true;
+            this.btnAbmGrado.Click += new System.EventHandler(this.btnAbmGrado_Click);
+            // 
+            // btnModificarContraseña
+            // 
+            this.btnModificarContraseña.Location = new System.Drawing.Point(144, 177);
+            this.btnModificarContraseña.Name = "btnModificarContraseña";
+            this.btnModificarContraseña.Size = new System.Drawing.Size(126, 23);
+            this.btnModificarContraseña.TabIndex = 11;
+            this.btnModificarContraseña.Text = "Modificar Contraseña";
+            this.btnModificarContraseña.UseVisualStyleBackColor = true;
+            this.btnModificarContraseña.Click += new System.EventHandler(this.btnModificarContraseña_Click);
+            // 
+            // btnRendicionComisiones
+            // 
+            this.btnRendicionComisiones.Location = new System.Drawing.Point(12, 177);
+            this.btnRendicionComisiones.Name = "btnRendicionComisiones";
+            this.btnRendicionComisiones.Size = new System.Drawing.Size(126, 23);
+            this.btnRendicionComisiones.TabIndex = 10;
+            this.btnRendicionComisiones.Text = "Rendir Comisiones";
+            this.btnRendicionComisiones.UseVisualStyleBackColor = true;
+            this.btnRendicionComisiones.Click += new System.EventHandler(this.btnRendicionComisiones_Click);
+            // 
+            // btnCanjePuntos
+            // 
+            this.btnCanjePuntos.Location = new System.Drawing.Point(144, 148);
+            this.btnCanjePuntos.Name = "btnCanjePuntos";
+            this.btnCanjePuntos.Size = new System.Drawing.Size(126, 23);
+            this.btnCanjePuntos.TabIndex = 9;
+            this.btnCanjePuntos.Text = "Canjear Puntos";
+            this.btnCanjePuntos.UseVisualStyleBackColor = true;
+            this.btnCanjePuntos.Click += new System.EventHandler(this.btnCanjePuntos_Click);
+            // 
+            // btnHistorial
+            // 
+            this.btnHistorial.Location = new System.Drawing.Point(12, 148);
+            this.btnHistorial.Name = "btnHistorial";
+            this.btnHistorial.Size = new System.Drawing.Size(126, 23);
+            this.btnHistorial.TabIndex = 8;
+            this.btnHistorial.Text = "Historial Cliente";
+            this.btnHistorial.UseVisualStyleBackColor = true;
+            this.btnHistorial.Click += new System.EventHandler(this.btnHistorial_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnModificarContraseña);
+            this.Controls.Add(this.btnRendicionComisiones);
+            this.Controls.Add(this.btnCanjePuntos);
+            this.Controls.Add(this.btnHistorial);
+            this.Controls.Add(this.btnComprar);
+            this.Controls.Add(this.btnEditarPublicacion);
+            this.Controls.Add(this.btnGenerarPublicacion);
+            this.Controls.Add(this.btnAbmGrado);
+            this.Controls.Add(this.btnAbmEmpresa);
+            this.Controls.Add(this.btnAbmClientes);
+            this.Controls.Add(this.btnRegistroUsuario);
+            this.Controls.Add(this.btnAbmRol);
+            this.Name = "Home";
             this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAbmRol;
+        private System.Windows.Forms.Button btnRegistroUsuario;
+        private System.Windows.Forms.Button btnAbmEmpresa;
+        private System.Windows.Forms.Button btnAbmClientes;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.Button btnEditarPublicacion;
+        private System.Windows.Forms.Button btnGenerarPublicacion;
+        private System.Windows.Forms.Button btnAbmGrado;
+        private System.Windows.Forms.Button btnModificarContraseña;
+        private System.Windows.Forms.Button btnRendicionComisiones;
+        private System.Windows.Forms.Button btnCanjePuntos;
+        private System.Windows.Forms.Button btnHistorial;
     }
 }
--- desktopApp/Views/Home.cs
+++ desktopApp/Views/Home.cs
@@ -16,5 +16,107 @@ namespace PalcoNet
         {
             InitializeComponent();
         }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            habilitarBotonesFuncionalidades();
+        }
+
+        public void habilitarBotonesFuncionalidades()
+        {
+
+        }
+
+        private void btnAbmRol_Click(object sender, EventArgs e)
+        {
+            new Roles.Listado().Show();
+            this.Hide();
+        }
+
+        private void btnRegistroUsuario_Click(object sender, EventArgs e)
+        {
+            new Usuarios.Signup().Show();
+            this.Hide();
+        }
+
+        private void btnAbmClientes_Click(object sender, EventArgs e)
+        {
+            new Clientes.Listado().Show();
+            this.Hide();
+        }
+
+        private void btnAbmEmpresa_Click(object sender, EventArgs e)
+        {
+            new Empresas.Listado().Show();
+            this.Hide();
+        }
+
+        private void btnAbmGrado_Click(object sender, EventArgs e)
+        {
+            new Grados.Listado().Show();
+            this.Hide();
+        }
+
+        private void btnGenerarPublicacion_Click(object sender, EventArgs e)
+        {
+            new Publicaciones.Alta().Show();
+            this.Hide();
+        }
+
+        private void btnEditarPublicacion_Click(object sender, EventArgs e)
+        {
+            // new Publicaciones.().Show(); Crear el formulario para editar una publicacion
+            //this.Hide();
+            MessageBox.Show("Todavia no esta implementado");
+        }
+
+        private void btnComprar_Click(object sender, EventArgs e)
+        {
+            // new Publicaciones.().Show(); Crear el formulario para comrar una entrada
+            //this.Hide();
+            MessageBox.Show("Todavia no esta implementado");
+        }
+
+        private void btnHistorial_Click(object sender, EventArgs e)
+        {
+            //Hay algo raro aca, solo te deja hacerlo desde clientess.Historial
+            //Se debe haber cagado algo cuando cambiamos todo de nombre
+            //new Clientes.Historial().Show();
+           // this.Hide();
+            MessageBox.Show("Hay un error con los nombres que no permite que funcione, chequear el codigo del onClick y ver el comentario");
+        }
+
+        private void btnCanjePuntos_Click(object sender, EventArgs e)
+        {
+            new Clientes.CanjearPuntos().Show();
+            this.Hide();
+        }
+
+        private void btnRendicionComisiones_Click(object sender, EventArgs e)
+        {
+            // new RendirComisiones.Show(); Crear el formulario para rendir comisiones
+            //this.Hide();
+            MessageBox.Show("Todavia no esta implementado");
+        }
+
+        private void btnModificarContraseña_Click(object sender, EventArgs e)
+        {
+            if (Global.rolUsuario == Model.TipoRol.Administrativo)
+            {
+                new Usuarios.ModificarClaveAdmin().Show();
+                this.Hide();
+            }
+            else if (Global.rolUsuario == Model.TipoRol.Cliente)
+            {
+                new Usuarios.ModificarClaveUsuario().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("El cambio de contraseña para un usuario con un rol que no sea cliente o administrativo no esta implementado");
+            }
+        }
+
+
     }
 }
