4b3fb1c1d3c1ee0e2efe656f18a25939b682e6c3
Kevin Szuchet <kevinszuchet@hotmail.com>
2018-11-08 20:26:32 -0300

    Agrego 2 clases de funciones utiles y algunos cambios en el listado de clientes

---
+++ desktopApp/Utils/DataGridViewUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PalcoNet.Utils
+{
+    class DataGridViewUtils
+    {
+
+        public static int? obtenerIdSeleccionado(DataGridView dgv) {
+            try
+            {
+                return int.Parse(dgv.Rows[dgv.CurrentRow.Index].Cells[0].Value.ToString());
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static void actualizarDataGriedView(DataGridView dgv, IQueryable<Object> query, string idOculto = null)
+        {
+            dgv.DataSource = query.ToList();
+            if (!string.IsNullOrWhiteSpace(idOculto))
+            {
+                dgv.Columns[idOculto].Visible = false;
+            }
+        }
+    }
+}
---
+++ desktopApp/Utils/MainUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace PalcoNet.Utils
+{
+    class MainUtils
+    {
+        public static void abrirFormulario(Form frm, Action callback) {
+            frm.ShowDialog();
+            callback();
+        }
+
+        public static void volverALaHome(Control actualContext) {
+            actualContext.Hide();
+            new Home().Show();
+        }
+    }
+}
