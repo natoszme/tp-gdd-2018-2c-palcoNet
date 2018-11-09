f14553d95ffa9e402bf0b057d5abfdfa65901218
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-11-08 21:03:50 -0300

    Creo el formulario que se usara para crear las ubicaciones

---
+++ desktopApp/Views/Publicaciones/GenerarUbicaciones.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PalcoNet.Views.Publicaciones
+{
+    partial class GenerarUbicaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFila = new System.Windows.Forms.TextBox();
+            this.txtAsiento = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.cbxNumerada = new System.Windows.Forms.CheckBox();
+            this.dgvUbicaciones = new System.Windows.Forms.DataGridView();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUbicaciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFila
+            // 
+            this.txtFila.Location = new System.Drawing.Point(54, 23);
+            this.txtFila.Name = "txtFila";
+            this.txtFila.Size = new System.Drawing.Size(100, 20);
+            this.txtFila.TabIndex = 0;
+            // 
+            // txtAsiento
+            // 
+            this.txtAsiento.Location = new System.Drawing.Point(54, 50);
+            this.txtAsiento.Name = "txtAsiento";
+            this.txtAsiento.Size = new System.Drawing.Size(100, 20);
+            this.txtAsiento.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(174, 38);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 40);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar Ubicacion";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(54, 77);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 3;
+            this.txtPrecio.Text = "Precio";
+            // 
+            // cbxNumerada
+            // 
+            this.cbxNumerada.AutoSize = true;
+            this.cbxNumerada.Location = new System.Drawing.Point(54, 130);
+            this.cbxNumerada.Name = "cbxNumerada";
+            this.cbxNumerada.Size = new System.Drawing.Size(124, 17);
+            this.cbxNumerada.TabIndex = 4;
+            this.cbxNumerada.Text = "Ubicacion numerada";
+            this.cbxNumerada.UseVisualStyleBackColor = true;
+            // 
+            // dgvUbicaciones
+            // 
+            this.dgvUbicaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUbicaciones.Location = new System.Drawing.Point(32, 154);
+            this.dgvUbicaciones.Name = "dgvUbicaciones";
+            this.dgvUbicaciones.Size = new System.Drawing.Size(240, 150);
+            this.dgvUbicaciones.TabIndex = 5;
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Location = new System.Drawing.Point(54, 103);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(100, 21);
+            this.cboTipo.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Fila";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Asiento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Precio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tipo";
+            // 
+            // GenerarUbicaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 319);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboTipo);
+            this.Controls.Add(this.dgvUbicaciones);
+            this.Controls.Add(this.cbxNumerada);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtAsiento);
+            this.Controls.Add(this.txtFila);
+            this.Name = "GenerarUbicaciones";
+            this.Text = "GenerarUbicaciones";
+            this.Load += new System.EventHandler(this.GenerarUbicaciones_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUbicaciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFila;
+        private System.Windows.Forms.TextBox txtAsiento;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.CheckBox cbxNumerada;
+        private System.Windows.Forms.DataGridView dgvUbicaciones;
+        private System.Windows.Forms.ComboBox cboTipo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ desktopApp/Views/Publicaciones/GenerarUbicaciones.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PalcoNet.Model;
+
+namespace PalcoNet.Views.Publicaciones
+{
+    public partial class GenerarUbicaciones : Form
+    {
+
+        List<Ubicacion> ubicaciones = new List<Ubicacion>();
+
+        public GenerarUbicaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string filaIngresada = txtFila.Text;
+            string asientoIngresado = txtAsiento.Text;
+            if (ubicacionValida())
+            {
+                MessageBox.Show("Ubicacion creada con exito");
+                
+                dgvUbicaciones.Update();
+                dgvUbicaciones.Refresh();
+                
+                
+            }
+        }
+
+        bool ubicacionValida()
+        {
+          
+            List<Control> inputs = new List<Control>();
+            inputs.Add(txtFila);
+            inputs.Add(txtAsiento);
+            inputs.Add(txtPrecio);
+            if (new Funciones_generales.Validaciones().todosNumericos(inputs))
+            {
+                if (new Funciones_generales.Validaciones().campoVacio(cboTipo))
+                {
+                    MessageBox.Show("Debe seleccionar un tipo de ubicacion");
+                }
+                else
+                {
+                    Ubicacion nuevaUbicacion = new Ubicacion(int.Parse(txtFila.Text), int.Parse(txtAsiento.Text), int.Parse(txtPrecio.Text), new Tipo_ubicacion(), cbxNumerada.Checked);
+                    if (ubicaciones.Any(ubicacion => esMismaUbicacion(ubicacion, nuevaUbicacion)))
+                    {
+                        MessageBox.Show("Esa ubicacion ya esta ingresada");
+                    }
+                    else
+                    {
+                        ubicaciones.Add(nuevaUbicacion);
+                        return true;
+                    }
+                   
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe ingresar campos numericos");
+            }
+            /*if (new Funciones_generales.Validaciones().campoVacio(txtFila))
+            {
+                MessageBox.Show("Campo vacio");
+            }
+            else
+            {
+                MessageBox.Show("No vacio");
+            }
+
+            if (new Funciones_generales.Validaciones().esNumerico(txtFila))
+            {
+                MessageBox.Show("Es numerico");
+            }
+            else
+            {
+                MessageBox.Show("No es numerico");
+            }*/
+
+
+            return false ;
+        }
+
+
+        bool esMismaUbicacion(Ubicacion existente, Ubicacion nuevaUbicacion)
+        {
+            return (existente.fila == nuevaUbicacion.fila && existente.asiento == nuevaUbicacion.asiento);
+        }
+
+        private void GenerarUbicaciones_Load(object sender, EventArgs e)
+        {
+            dgvUbicaciones.DataSource = ubicaciones;
+        }
+
+     
+
+        
+        
+
+     
+
+    }
+}
