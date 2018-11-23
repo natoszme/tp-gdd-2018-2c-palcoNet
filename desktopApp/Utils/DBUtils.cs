16763844fb3219862631ed8d97c6f3c96b6a506a
natoszme <natoszmedra@gmail.com>
2018-11-23 18:16:58 -0300

    Revert "Saco una clase que ya no se usa. Agrego comentarios y saco codigo comentado"
    This reverts commit 5c012b3ac932e00785bf194bd0ad2c566559f136.

---
+++ desktopApp/Utils/DBUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PalcoNet.Model;
+using System.Data.Entity.Validation;
+
+namespace PalcoNet.Utils
+{
+    class DBUtils {
+        
+    }
+}
