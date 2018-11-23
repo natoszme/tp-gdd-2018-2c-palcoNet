a9391f10b3773107afe1917f4ecfa6c848bf4323
Kevin Szuchet <kevinszuchet@hotmail.com>
2018-11-23 19:52:06 -0300

    Agrego el cambio de claves como usuario

--- desktopApp/Views/Usuarios/ModificarClaveUsuario.cs
+++ desktopApp/Views/Usuarios/ModificarClaveUsuario.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PalcoNet.Model;
+using PalcoNet.Utils;
 
 namespace PalcoNet.Usuarios
 {
@@ -17,15 +19,56 @@ namespace PalcoNet.Usuarios
             InitializeComponent();
         }
 
-        private void btnCambiar_Click(object sender, EventArgs e)
-        {
-            this.Hide();
+        #region VALIDACIONES
+        private bool camposValidos() {
+            bool camposValidos = true;
+            try {
+                ValidationsUtils.campoObligatorio(txtClaveActual, "contraseña actual");
+                ValidationsUtils.campoObligatorio(txtNuevaClave, "nueva contraseña");
+                ValidationsUtils.campoObligatorio(txtRepetirClave, "repetir contraseña");
+                ValidationsUtils.clavesCoincidentes(txtNuevaClave, txtRepetirClave);
+            } catch (ValidationException e) {
+                WindowsFormUtils.mensajeDeError(e.Message);
+                camposValidos = false;
+            }
+            return camposValidos;
         }
+
+        private bool validarDominio() {
+            try {
+                claveActualExistente();
+            } catch (ValidationException e) {
+                WindowsFormUtils.mensajeDeError(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        private void claveActualExistente() {
+            Usuario usuarioDB = BaseDeDatos.BaseDeDatos.obtenerUsuarioPorCredenciales(Global.usuarioLogueado.usuario, txtClaveActual.Text);
+            if (usuarioDB == null || usuarioDB.id_usuario != Global.usuarioLogueado.id_usuario) {
+                throw new ValidationException("La clave actual ingresada es incorrecta");
+            }
+        }
+        #endregion
 
         private void btnVolver_Click(object sender, EventArgs e)
         {
             this.Hide();
             new Home().Show();
+        }
+
+        private void btnCambiar_Click(object sender, EventArgs e)
+        {
+
+            if (camposValidos())
+            {
+
+                if (!validarDominio()) return;
+
+                Global.usuarioLogueado.clave = txtNuevaClave.Text;
+                WindowsFormUtils.guardarYCerrar(this);
+            }
         }
     }
 }
