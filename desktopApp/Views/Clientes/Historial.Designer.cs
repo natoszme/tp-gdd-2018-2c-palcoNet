d2e7e924faa4fabc24597e6886c6f55f16700ac2
natoszme <natoszmedra@gmail.com>
2018-11-25 07:12:13 -0300

    Empiezo con Historial de compra de cliente

--- desktopApp/Views/Clientes/Historial.Designer.cs
+++ desktopApp/Views/Clientes/Historial.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.btnVolver = new System.Windows.Forms.Button();
+            this.dgvCompras = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompras)).BeginInit();
             this.SuspendLayout();
             // 
             // btnVolver
             // 
-            this.btnVolver.Location = new System.Drawing.Point(197, 227);
+            this.btnVolver.Location = new System.Drawing.Point(409, 227);
             this.btnVolver.Name = "btnVolver";
             this.btnVolver.Size = new System.Drawing.Size(75, 23);
             this.btnVolver.TabIndex = 0;
@@ -41,14 +43,25 @@
             this.btnVolver.UseVisualStyleBackColor = true;
             this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
             // 
+            // dgvCompras
+            // 
+            this.dgvCompras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCompras.Location = new System.Drawing.Point(12, 24);
+            this.dgvCompras.Name = "dgvCompras";
+            this.dgvCompras.Size = new System.Drawing.Size(464, 182);
+            this.dgvCompras.TabIndex = 1;
+            // 
             // Historial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(496, 262);
+            this.Controls.Add(this.dgvCompras);
             this.Controls.Add(this.btnVolver);
             this.Name = "Historial";
             this.Text = "Historial";
+            this.Load += new System.EventHandler(this.Historial_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompras)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.DataGridView dgvCompras;
     }
 }
--- desktopApp/Views/Clientes/Historial.cs
+++ desktopApp/Views/Clientes/Historial.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PalcoNet.Model;
+using PalcoNet.Utils;
 
 namespace PalcoNet.Clientes
 {
@@ -21,6 +23,24 @@ namespace PalcoNet.Clientes
         {
             this.Hide();
             new Home().Show();
+        }
+
+        private void Historial_Load(object sender, EventArgs e)
+        {
+            using (RagnarEntities db = new RagnarEntities())
+            {
+                IQueryable<Compra> comprasDeCliente = db.Cliente.Find(Global.usuarioLogueado.id_usuario).Compra.AsQueryable();
+
+                var compras = comprasDeCliente.Select(c => new
+                {
+                    //TODO agregar el total de la compra
+                    razon_social = c.Empresa.razon_social,
+                    tarjeta_utilizada = c.tarjeta_utilizada,
+                    fecha = c.fecha
+                }).OrderBy(c => c.fecha);
+
+                DataGridViewUtils.actualizarDataGriedView(dgvCompras, compras, "id_compra");
+            }
         }
     }
 }
