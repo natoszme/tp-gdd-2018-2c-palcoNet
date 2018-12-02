8f971e515ffcddadf3bec293a1fd361cdf56f5a3
natoszme <natoszmedra@gmail.com>
2018-12-02 19:17:47 -0300

    Base de Rendicion de comisones. Falta acutalizar el modelo para cerrarlo

---
+++ TP2C2018 K3522 RAGNAR 20/src/desktopApp/Views/RendicionComisiones/Listado.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PalcoNet.Views.RendicionComisiones
+{
+    partial class Listado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvItemsFacturas = new System.Windows.Forms.DataGridView();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtComprasARendir = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItemsFacturas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(558, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar rendición de comisiones";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvItemsFacturas
+            // 
+            this.dgvItemsFacturas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvItemsFacturas.Location = new System.Drawing.Point(13, 62);
+            this.dgvItemsFacturas.Name = "dgvItemsFacturas";
+            this.dgvItemsFacturas.Size = new System.Drawing.Size(725, 285);
+            this.dgvItemsFacturas.TabIndex = 1;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(663, 362);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(75, 23);
+            this.btnVolver.TabIndex = 2;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese la cantidad de compras a rendir";
+            // 
+            // txtComprasARendir
+            // 
+            this.txtComprasARendir.Location = new System.Drawing.Point(226, 21);
+            this.txtComprasARendir.Name = "txtComprasARendir";
+            this.txtComprasARendir.Size = new System.Drawing.Size(77, 20);
+            this.txtComprasARendir.TabIndex = 4;
+            // 
+            // Listado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 397);
+            this.Controls.Add(this.txtComprasARendir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.dgvItemsFacturas);
+            this.Controls.Add(this.button1);
+            this.Name = "Listado";
+            this.Text = "Listado";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItemsFacturas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvItemsFacturas;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtComprasARendir;
+    }
+}
---
+++ TP2C2018 K3522 RAGNAR 20/src/desktopApp/Views/RendicionComisiones/Listado.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PalcoNet.Model;
+using PalcoNet.Utils;
+
+namespace PalcoNet.Views.RendicionComisiones
+{
+    public partial class Listado : Form
+    {
+        public Listado()
+        {
+            InitializeComponent();
+            actualizarDataGridView(new RagnarEntities());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (camposValidos())
+            {
+                using (RagnarEntities db = new RagnarEntities())
+                {
+                    //TODO sacar cuando se actualice el modeo
+                    //db.SP_RendicionDeComisiones(int.Parse(txtComprasARendir.Text), Global.fechaDeHoy());
+                    actualizarDataGridView(db);
+                }
+            }
+        }
+
+        private bool camposValidos()
+        {
+            bool camposValidos = true;
+            try
+            {
+                ValidationsUtils.campoObligatorio(txtComprasARendir, "compras a rendir");
+                ValidationsUtils.campoEnteroYPositivo(txtComprasARendir, "compras a rendir");
+                if (int.Parse(txtComprasARendir.Text) == 0)
+                {
+                    throw new ValidationException("La cantidad de compras a rendir debe ser mayor a 0");
+                }
+            }
+            catch (ValidationException e)
+            {
+                WindowsFormUtils.mensajeDeError(e.Message);
+                camposValidos = false;
+            }
+            return camposValidos;
+        }
+
+        private void actualizarDataGridView(RagnarEntities db)
+        {
+            IQueryable<Item_factura> itemsFacturas = db.Item_factura.AsQueryable();
+
+            var facturasFiltradas =
+                itemsFacturas
+                .Select(itemFactura => new
+                {
+                    id_item_factura = itemFactura.id_item,
+                    numeroDeFactura = itemFactura.Factura.numero,
+                    fechaFactura = itemFactura.Factura.fecha,
+                    formaDePago = itemFactura.Factura.forma_pago,
+                    descripcion = itemFactura.descripcion,
+                    precioUbicacion = itemFactura.Ubicacion_publicacion.precio,                    
+                    comision = itemFactura.Ubicacion_publicacion.precio * itemFactura.Ubicacion_publicacion.Publicacion.Grado_publicacion.comision
+                })
+                .OrderByDescending(itemFactura => itemFactura.fechaFactura);
+
+            DataGridViewUtils.actualizarDataGriedView(dgvItemsFacturas, facturasFiltradas, "id_item_factura");          
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            WindowsFormUtils.volverALaHome(this);
+        }
+    }
+}
