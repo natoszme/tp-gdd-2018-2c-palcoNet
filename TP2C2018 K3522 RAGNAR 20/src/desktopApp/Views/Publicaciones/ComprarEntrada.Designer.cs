b620e884112980b2ebf0ff0861559ea66b48b066
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-12-06 16:34:50 -0300

    Se muestran las publicaciones en el dgv

--- TP2C2018 K3522 RAGNAR 20/src/desktopApp/Views/Publicaciones/ComprarEntrada.Designer.cs
+++ TP2C2018 K3522 RAGNAR 20/src/desktopApp/Views/Publicaciones/ComprarEntrada.Designer.cs
@@ -29,13 +29,19 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dgvEspectaculos = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEspectaculos)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -46,22 +52,6 @@
             this.panel1.Size = new System.Drawing.Size(510, 39);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Location = new System.Drawing.Point(12, 80);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(510, 64);
-            this.panel2.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(13, 31);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -71,14 +61,18 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Compras";
             // 
-            // label2
+            // panel2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 72);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Filtros";
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 80);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(510, 64);
+            this.panel2.TabIndex = 1;
             // 
             // label3
             // 
@@ -89,20 +83,80 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Compras";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Filtros";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Compras";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Compras";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(225, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // dgvEspectaculos
+            // 
+            this.dgvEspectaculos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEspectaculos.Location = new System.Drawing.Point(12, 171);
+            this.dgvEspectaculos.Name = "dgvEspectaculos";
+            this.dgvEspectaculos.Size = new System.Drawing.Size(240, 150);
+            this.dgvEspectaculos.TabIndex = 2;
+       
+            // 
             // ComprarEntrada
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(544, 407);
+            this.Controls.Add(this.dgvEspectaculos);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "ComprarEntrada";
             this.Text = "ComprarEntrada";
+            this.Load += new System.EventHandler(this.ComprarEntrada_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEspectaculos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,5 +170,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dgvEspectaculos;
     }
 }
--- TP2C2018 K3522 RAGNAR 20/src/desktopApp/Views/Publicaciones/ComprarEntrada.cs
+++ TP2C2018 K3522 RAGNAR 20/src/desktopApp/Views/Publicaciones/ComprarEntrada.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PalcoNet.Model;
+using PalcoNet.Utils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +18,60 @@ namespace PalcoNet.Views.Publicaciones
         {
             InitializeComponent();
         }
+
+        private void ComprarEntrada_Load(object sender, EventArgs e)
+        {
+            actualizarDataGriedView();
+            dgvEspectaculos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+
+        #region HELPER
+        private void actualizarDataGriedView()
+        {
+            using (RagnarEntities db = new RagnarEntities())
+            {
+
+                IQueryable<Publicacion> espectaculosTotales = db.Publicacion.AsQueryable();
+
+                /*if (!string.IsNullOrWhiteSpace(txtNombre.Text))
+                {
+                    clientesFiltrados = clientesFiltrados.Where(c => c.nombre.Contains(txtNombre.Text));
+                }
+
+                if (!string.IsNullOrWhiteSpace(txtApellido.Text))
+                {
+                    clientesFiltrados = clientesFiltrados.Where(c => c.apellido.Contains(txtApellido.Text));
+                }
+
+                if (!string.IsNullOrWhiteSpace(txtEmail.Text))
+                {
+                    clientesFiltrados = clientesFiltrados.Where(c => c.mail.Contains(txtEmail.Text));
+                }
+
+                if (!string.IsNullOrWhiteSpace(txtTipoDocumento.Text))
+                {
+                    clientesFiltrados = clientesFiltrados.Where(c => c.tipo_documento.ToString() == txtTipoDocumento.Text);
+                }
+
+                if (!string.IsNullOrWhiteSpace(txtDocumento.Text))
+                {
+                    clientesFiltrados = clientesFiltrados.Where(c => c.numero_documento.ToString() == txtDocumento.Text);
+                }*/
+
+                var espectaculos = espectaculosTotales.Select(c => new
+                {
+                    id_publicacion = c.id_publicacion,
+                    descripcion = c.descripcion,
+                    direccion = c.direccion,
+                    fecha_espectaculo = c.fecha_espectaculo,
+                    stock = c.stock,
+                    rubro = c.Rubro
+                })/*.OrderBy(c => c.nombre).ThenBy(c => c.apellido)*/;
+
+                DataGridViewUtils.actualizarDataGriedView(dgvEspectaculos, espectaculos, "id_publicacion");
+            }
+        }
+        #endregion
     }
 }
