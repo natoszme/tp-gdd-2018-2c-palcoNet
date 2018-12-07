4392bf93f307c46cf26975f0c01c8da93cb04289
Kevin Szuchet <kevinszuchet@hotmail.com>
2018-12-06 22:29:36 -0300

    Agrego la parte del paginado en el listado de clientes

---
+++ TP2C2018 K3522 RAGNAR 20/src/desktopApp/Utils/Paginador.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Windows.Forms;
+
+namespace PalcoNet.Utils
+{
+    class Paginador
+    {
+        /*
+         * Se instancia en el loading del listado que lo vaya a actualizar con la cantidad de registros obtenidos en total
+         * Se crean los eventos (onlick) de los 4 botones y se los llama dentro del form
+         * Se pasa tambien el lbl para cambiar el numero de la pagina actual
+         * Es importante usar el Skip y Take en la query donde se obtienen los registros, utilizando los metodos INIT y LIMIT
+         * */
+
+        private int pageSize { get; set; }
+        private int totalRecords { get; set; }
+
+        private int currentPage;
+        private int totalPages { get; set;  }
+
+        private Label lblCurrentPage;
+
+        public Paginador(int pageSize, int totalRecords, Label lblCurrentPage) {
+            this.pageSize = pageSize;
+            this.totalRecords = totalRecords;
+            this.currentPage = 1;
+
+            this.totalPages = (int) Math.Ceiling((double) totalRecords / (double) pageSize);
+
+            this.lblCurrentPage = lblCurrentPage;
+        }
+
+        public int CurrentPage
+        {
+            get { return currentPage; }
+            set { 
+                currentPage = value; 
+                lblCurrentPage.Text = currentPage.ToString();  
+            }
+        }
+
+        public void restart() {
+            this.first();
+        }
+
+        public void first() {
+            CurrentPage = 1;
+        }
+
+        public void prev() {
+            CurrentPage = Math.Max(1, currentPage - 1);
+        }
+
+        public void next() {
+            CurrentPage = Math.Min(totalPages, currentPage + 1);
+        }
+
+        public void last() {
+            CurrentPage = totalPages;
+        }
+
+        public int init() {
+            return (currentPage - 1) * pageSize;
+        }
+
+        public int limit() {
+            return pageSize;
+        }
+    }
+}
--- TP2C2018 K3522 RAGNAR 20/src/desktopApp/Views/Clientes/Listado.Designer.cs
+++ TP2C2018 K3522 RAGNAR 20/src/desktopApp/Views/Clientes/Listado.Designer.cs
@@ -47,9 +47,16 @@
             this.txtApellido = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnVolver = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnUltima = new System.Windows.Forms.Button();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.lblPaginaActual = new System.Windows.Forms.Label();
+            this.btnPrimera = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvClientes
@@ -228,7 +235,7 @@
             // 
             // btnVolver
             // 
-            this.btnVolver.Location = new System.Drawing.Point(770, 364);
+            this.btnVolver.Location = new System.Drawing.Point(770, 411);
             this.btnVolver.Name = "btnVolver";
             this.btnVolver.Size = new System.Drawing.Size(75, 23);
             this.btnVolver.TabIndex = 9;
@@ -236,11 +243,73 @@
             this.btnVolver.UseVisualStyleBackColor = true;
             this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnUltima);
+            this.panel2.Controls.Add(this.btnSiguiente);
+            this.panel2.Controls.Add(this.btnAnterior);
+            this.panel2.Controls.Add(this.lblPaginaActual);
+            this.panel2.Controls.Add(this.btnPrimera);
+            this.panel2.Location = new System.Drawing.Point(12, 351);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(833, 45);
+            this.panel2.TabIndex = 10;
+            // 
+            // btnUltima
+            // 
+            this.btnUltima.Location = new System.Drawing.Point(465, 9);
+            this.btnUltima.Name = "btnUltima";
+            this.btnUltima.Size = new System.Drawing.Size(30, 26);
+            this.btnUltima.TabIndex = 4;
+            this.btnUltima.Text = ">>";
+            this.btnUltima.UseVisualStyleBackColor = true;
+            this.btnUltima.Click += new System.EventHandler(this.btnUltima_Click);
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(429, 9);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(30, 26);
+            this.btnSiguiente.TabIndex = 3;
+            this.btnSiguiente.Text = ">";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Location = new System.Drawing.Point(374, 9);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(30, 26);
+            this.btnAnterior.TabIndex = 2;
+            this.btnAnterior.Text = "<";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            this.btnAnterior.Click += new System.EventHandler(this.btnAnterior_Click);
+            // 
+            // lblPaginaActual
+            // 
+            this.lblPaginaActual.AutoSize = true;
+            this.lblPaginaActual.Location = new System.Drawing.Point(410, 16);
+            this.lblPaginaActual.Name = "lblPaginaActual";
+            this.lblPaginaActual.Size = new System.Drawing.Size(13, 13);
+            this.lblPaginaActual.TabIndex = 1;
+            this.lblPaginaActual.Text = "1";
+            // 
+            // btnPrimera
+            // 
+            this.btnPrimera.Location = new System.Drawing.Point(338, 9);
+            this.btnPrimera.Name = "btnPrimera";
+            this.btnPrimera.Size = new System.Drawing.Size(30, 26);
+            this.btnPrimera.TabIndex = 0;
+            this.btnPrimera.Text = "<<";
+            this.btnPrimera.UseVisualStyleBackColor = true;
+            this.btnPrimera.Click += new System.EventHandler(this.btnPrimera_Click);
+            // 
             // Listado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(857, 399);
+            this.ClientSize = new System.Drawing.Size(857, 446);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.btnVolver);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dgvClientes);
@@ -253,6 +322,8 @@
             this.panel1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -278,5 +349,11 @@
         private System.Windows.Forms.Button btnVolver;
         private System.Windows.Forms.TextBox txtTipoDocumento;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnUltima;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Label lblPaginaActual;
+        private System.Windows.Forms.Button btnPrimera;
     }
 }
